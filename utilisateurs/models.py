b19922ed0029d4073a6c0391c0dc3c21df5727e4
unknown <chpoutgnouf@hotmail.fr>
2020-01-16 22:04:19 +0100

    recorrect staticfiles

--- utilisateurs/models.py
+++ utilisateurs/models.py
@@ -2,10 +2,27 @@ from django.db import models
 
 
 class Volontaire(models.Model):
-	est_unis = models.BooleanField(default=False)
+
+	"""	groupes=choices('Non Unis'
+			'Connectes/emplois',
+			'JCN Chèque énergie',
+			'Colomiers',
+			'Cinema et citoyenneté',
+			
+			)
+	"""
+	GROUPE_CHOICES = [
+		
+		('01', 'Connectés/Emploi'),
+		('02', 'JCN/Chèque Energie'),
+		('03', 'Colomiers'),
+		('04', 'Cinéma et citoyenneté'),
+		('na', 'Non Unis'),
+	]
 
 	prenom = models.CharField(max_length=30)
 	nom = models.CharField(max_length=30)
+	groupe = models.CharField(max_length=2, choices=GROUPE_CHOICES, default='01' )
 	
 	tel = models.CharField(max_length=10, blank=True, null=True)
 	mail = models.EmailField(blank=True, null=True)
